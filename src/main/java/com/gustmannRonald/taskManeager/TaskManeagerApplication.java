2c1b5d7f82f20a0e988b06f6c88d4aa65be3f01c
RonaldGustmann <ronald29eduardo@gmail.com>
2025-04-25 00:45:37 -0300

    initial commit

---
+++ src/main/java/com/gustmannRonald/taskManeager/TaskManeagerApplication.java
@@ -0,0 +1,13 @@
+package com.gustmannRonald.taskManeager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManeagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManeagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustmannRonald/taskManeager/TaskManeagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustmannRonald.taskManeager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManeagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
